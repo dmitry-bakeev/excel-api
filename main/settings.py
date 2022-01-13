5f15c33d30ea7bed7f2c2e1560de70e803cec6ff
dmitry-bakeev <bakeevdimon@gmail.com>
2022-01-13 13:47:38 +0500

    Add media settings

--- main/settings.py
+++ main/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_DIR = BASE_DIR / 'static'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
@@ -129,6 +130,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media
+
+MEDIA_URL = '/media/'
 
 
 # Dajngo Rest Framework
--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
@@ -12,3 +13,12 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns = static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    ) + urlpatterns
